9c91ea1248b561bf335796a6f5f2930166e5dfb7
GustavoMSandoval <148093617+GustavoMSandoval@users.noreply.github.com>
2024-04-27 19:16:04 -0300

    Add files via upload

---
+++ Aps-Java/src/loginandsignup/Connection.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginandsignup;
+
+import com.mysql.cj.xdevapi.Statement;
+
+/**
+ *
+ * @author Gustavo
+ */
+class Connection {
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
